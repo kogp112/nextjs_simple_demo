30f05159af29fb19cfda8c1bc7c02fde4962ed0d
gappi <gapkana@gmail.com>
2019-11-12 18:46:00 +0900

    modify tab space in Header.js

--- components/Header.js
+++ components/Header.js
@@ -13,24 +13,24 @@ const linkStyleAsia = {
 };
 
 const Header = () => (
-	<div>
-		<Link href="/">
-			<a style={linkStyleTop}>Top</a>
-		</Link>
-		<Link href={{ pathname: '/area', query: { name: 'Europe', country1: 'Barcelona', country2: 'Paris' }}}>
-			<a style={linkStyleEurope}>Europe</a>
+  <div>
+    <Link href="/">
+      <a style={linkStyleTop}>Top</a>
     </Link>
-		<Link href={{ pathname: '/area', query: { name: 'Asia', country1: 'Thailand', country2: 'Taiwan' }}}>
-			<a style={linkStyleAsia}>Asia</a>
+    <Link href={{ pathname: '/area', query: { name: 'Europe', country1: 'Barcelona', country2: 'Paris' }}}>
+      <a style={linkStyleEurope}>Europe</a>
     </Link>
-		<style jsx>{`
-        a {
-					margin-right: 15px;
-					font-weight: bold;
-          font-family: 'Meiryo';
-        }
-      `}</style>
-	</div>
+    <Link href={{ pathname: '/area', query: { name: 'Asia', country1: 'Thailand', country2: 'Taiwan' }}}>
+      <a style={linkStyleAsia}>Asia</a>
+    </Link>
+    <style jsx>{`
+      a {
+        margin-right: 15px;
+        font-weight: bold;
+        font-family: 'Meiryo';
+      }
+    `}</style>
+  </div>
 );
 
 export default Header;
