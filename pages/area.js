cd61e15f34ba0ff08c71c554be9dcdb903162996
gappi <gapkana@gmail.com>
2019-11-12 18:51:02 +0900

    modify tab space in area.js

--- pages/area.js
+++ pages/area.js
@@ -4,42 +4,38 @@ import Header from '../components/Header';
 
 const Area = () => {
   const router = useRouter();
-	
   return (
-      <div>
-				<Header />
-        <h1>{router.query.name}</h1>
-					<ul>
-					<li>
-						<Link href={{ pathname: '/country', query: { name: router.query.country1 }}}>
-							<a>{router.query.country1}</a>
-						</Link>
-					</li>
-					<li>
-						<Link href={{ pathname: '/country', query: { name: router.query.country2 }}}>
-							<a>{router.query.country2}</a>
-						</Link>
-					</li>
-      	</ul>
-				<style jsx>{`
+    <div>
+      <Header />
+      <h1>{router.query.name}</h1>
+      <ul>
+        <li>
+          <Link href={{ pathname: '/country', query: { name: router.query.country1 }}}>
+            <a>{router.query.country1}</a>
+          </Link>
+        </li>
+        <li>
+          <Link href={{ pathname: '/country', query: { name: router.query.country2 }}}>
+            <a>{router.query.country2}</a>
+          </Link>
+        </li>
+      </ul>
+      <style jsx>{`
         h1,a {
           font-family: 'Meiryo';
-				}
-				
-				ul a {
-					color: gray;
-				}
-				
-				a {
-					text-decoration: none;
-				}
-
+        }
+        ul a {
+          color: gray;
+        }
+        a {
+          text-decoration: none;
+        }
         li {
-          margin: 5px 0;
+        margin: 5px 0;
           padding-left: 10px;
         }
       `}</style>
-      </div>
+    </div>
   );
 };
 
